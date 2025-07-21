0f86ec203000d63231d71dc53b9009628b1319c0
Tomáš Hozza <thozza@redhat.com>
2025-07-21 19:56:46 +0200

    pkg/cloud/awscloud: switch S3 to v2 SDK
    The change is based on osbuild-composer code and commit [1], with some
    modifications. Specifically:
     - Some files and types are named a bit differently.
     - Unit testing approach is different than in osbuild-composer and
       aligns more with the unit tests implementation for awscloud.Uploader.
    
    [1] https://github.com/osbuild/osbuild-composer/commit/8d158f60314684cc8b1cbc5e5d39029b0e391c33
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- pkg/cloud/awscloud/export_test.go
+++ pkg/cloud/awscloud/export_test.go
@@ -2,6 +2,14 @@ package awscloud
 
 type AwsClient = awsClient
 
+func NewAWSForTest(s3cli S3, upldr S3Uploader, sign S3Presign) *AWS {
+	return &AWS{
+		s3:         s3cli,
+		s3uploader: upldr,
+		s3presign:  sign,
+	}
+}
+
 func MockNewAwsClient(f func(string) (awsClient, error)) (restore func()) {
 	saved := newAwsClient
 	newAwsClient = f
